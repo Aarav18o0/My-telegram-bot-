80f1a96fb2f9672ef5d0e398fb0a63f3740e6321
Aarav18o0 <sharmadevendra0169@gmail.com>
2026-07-03 10:59:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -160,9 +160,6 @@ async def daily_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     conn.commit()
 
-    await update.message.reply_text("🎉 Mubarak ho! Aapko 500 Coins mil gaye. 💰")
-
-    )
 async def message_filter(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.strip().lower()
     chat_id = update.effective_chat.id
